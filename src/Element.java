8f99dc82895680a4921e498cc62a9b6595e8c806
OdarkaBruh <trentidarey@gmail.com>
2026-05-05 16:21:20 +0300

    figured out

--- src/Element.java
+++ src/Element.java
@@ -1,60 +1,44 @@
 import java.util.ArrayList;
+import java.util.stream.IntStream;
 
 public class Element {
     private static final char[] actions = new char[]{'*', '/', '+', '-'};
     public static ArrayList<Element> elements = new ArrayList<>();
 
-    int priority = 0;
-    char actionBranch;
+    int priority;
+    int element;
+    char actionBranch = 0;
+
     Element leftElement;
     Element rightElement;
     Element parent;
 
-    Element(String value) {
-        System.out.println(value);
-        elements.add(this);
-        int index;
-        if ((index = getHighestAction(value)) != -1) {
-            actionBranch = value.charAt(index);
-            rightElement = new Element(value.substring(0,index+1));
-            leftElement = new Element(value.substring(index+1));
-        }
-    }
+    Element(String value, Element leftElement, int priority) {
+        System.out.println("V: "+ value);
+        this.leftElement = leftElement;
+        this.priority = priority;
 
-    private int countActions(String value) {
-        int i = 0;
-        for (char c : value.toCharArray()) {
-            System.out.println(i);
-            for (char a : actions) {
-                if (a == c) {
-                    i++;
-                    break;
-                }
+        elements.add(this);
+
+        for (int i = 0; i < value.length(); i++) {
+            if (value.charAt(i) == '(') this.priority+=2;
+            else if (value.charAt(i) == ')') this.priority-=2;
+            else if (Character.isDigit(value.charAt(i))) element = value.charAt(i);
+            else if (value.charAt(i) == ' ') continue;
+            else {
+                actionBranch = value.charAt(i);
+                rightElement = new Element(value.substring(i+1), this, this.priority);
+                break;
             }
         }
-        return i;
-    }
-
-    private void splitValue(String value) {
-        int index;
-        if ((index = value.lastIndexOf('('))!=-1){
-            index += getHighestAction(value.substring(index, value.indexOf(')')));
-
-            parent = new Element(value.substring(index-1, value.indexOf(')')));
-        }
-        //this.actionBranch = index;
-
     }
 
     private int getHighestAction(String value) {
         int index = 0;
         if ((index = value.lastIndexOf('('))!=-1){
-            System.out.println("BEFORE: "+value);
             value = value.substring(index+1);
             value = value.substring(0, value.indexOf(')'));
-            System.out.println("AFTER: "+value);
         }
-        System.out.println("!!");
         for (char c: actions) {
             for (int i = 0; i < value.length(); i++) {
                 if (value.charAt(i) == c) {
@@ -69,7 +53,20 @@ public class Element {
 
     @Override
     public String toString() {
-        return actionBranch + " " + leftElement + " " + rightElement;
+        StringBuilder sb = new StringBuilder();
+        if (actionBranch != 0) sb.append(actionBranch);
+        sb.append(" (");
+        sb.append(priority).append(")      : " );
+        //if (leftElement != null) sb.append(leftElement);
+        //sb.append("   |   ");
+        return sb.toString();
+    }
+
+    private boolean isAction(char check) {
+        for (char c: actions) {
+            if (check == c) return true;
+        }
+        return false;
     }
 }
 
--- src/Main2.java
+++ src/Main2.java
@@ -9,7 +9,10 @@ public class Main2 {
     public static void main(String[] args) {
         String n1 = "1*3+4";
         String n2 = "1*(3+(4*9)/7)";
-        Element e = new Element(n2);
+        new Element(n2, null, 0);
+        for (Element e: Element.elements) {
+            System.out.println(e);
+        }
     }
 }
 
